8fa09dc110ef27e833fa0cf6e4dad1020931b9cf
Nick Pitrak <pitrak1@gmail.com>
2020-03-23 07:07:34 -0500

    add start of turn tracking

--- src/client/states/game/game_state.py
+++ src/client/states/game/game_state.py
@@ -13,6 +13,7 @@ class GameState(state.State):
 		self.__rooms = client_room_grid.ClientRoomGrid()
 		self._elements = [self.__rooms]
 		self.__players = []
+		self.__current_player = False
 		self._add_command(command.Command('network_get_player_positions', { 'status': 'pending' }))
 		
 	def client_redraw_handler(self, command, state=None):
@@ -20,6 +21,19 @@ class GameState(state.State):
 		self.__groups = [pyglet.graphics.OrderedGroup(i) for i in range(constants.NUMBER_OF_GROUPS)]
 		command.data.update({ 'asset_manager': self._data['assets'], 'batch': self._batch, 'groups': self.__groups })
 		self.__rooms.on_command(command, state)
+		if self.__title:
+			self.__title_label = label.Label(
+				text=self.__title, 
+				x=constants.WINDOW_CENTER_X, 
+				y=constants.WINDOW_HEIGHT - 40, 
+				anchor_x='center', 
+				anchor_y='center', 
+				align='center', 
+				font_size=25, 
+				color=(255, 255, 255, 255),
+				batch=self._batch,
+				group=self.__groups[constants.HIGHLIGHTS_GROUP]
+			)
 
 	def set_player_positions(self, players):
 		for player_tuple in players:
@@ -32,8 +46,17 @@ class GameState(state.State):
 			)
 			self.__players.append(player)
 			self.__rooms.add_player(int(grid_x), int(grid_y), player)
-			self._add_command(command.Command('client_redraw'))
+			self._add_command(command.Command('network_get_current_player', { 'status': 'pending' }))
 
+	def set_current_player(self, player_name):
+		if player_name == 'self':
+			self.__title = 'Your turn'
+			self.__current_player = True
+		else:
+			self.__title = f'{player_name}\'s turn'
+			self.__current_player = False
+
+		self._add_command(command.Command('client_redraw'))
 
 	def select(self, node):
 		self._add_command(command.Command('client_select', { 'selected': node }))
--- src/server/states/game_state.py
+++ src/server/states/game_state.py
@@ -6,6 +6,7 @@ import config
 class GameState(state_module.State):
 	def __init__(self, data, set_state, add_command):
 		super().__init__(data, set_state, add_command)
+		self.current_player_index = 0
 		self.rooms = server_room_grid_module.ServerRoomGrid()
 		for player in self.players:
 			self.rooms.add_player(0, 0, player)
@@ -14,3 +15,10 @@ class GameState(state_module.State):
 	def network_get_player_positions_handler(self, command, state=None):
 		parsed_players = [(player.name, player.variable_name, player.grid_x, player.grid_y) for player in self.players]
 		command_module.update_and_send(command, { 'status': 'success', 'players': parsed_players })
+
+	def network_get_current_player_handler(self, command, state=None):
+		if command.data['connection'] == self.players[self.current_player_index]:
+			player_name = 'self'
+		else:
+			player_name = self.players[self.current_player_index].name
+		command_module.update_and_send(command, { 'status': 'success', 'player_name': player_name })
